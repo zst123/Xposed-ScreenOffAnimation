4595e6d48e55f1d007c318a611b2a8b4fc034a39
zst123 <manzel.seet@gmail.com>
2014-05-26 18:31:34 +0800

    Fix Navigation Bar still showing while animating on Android 4.4

--- src/com/zst/xposed/screenoffanimation/anim/ScreenOffAnim.java
+++ src/com/zst/xposed/screenoffanimation/anim/ScreenOffAnim.java
@@ -7,9 +7,12 @@ import com.zst.xposed.screenoffanimation.helpers.Utils;
 import android.content.Context;
 import android.content.res.Resources;
 import android.graphics.PixelFormat;
+import android.os.Build;
 import android.os.Handler;
 import android.os.PowerManager;
 import android.os.SystemClock;
+import android.util.DisplayMetrics;
+import android.view.Gravity;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.FrameLayout;
@@ -67,7 +70,27 @@ public abstract class ScreenOffAnim {
 		mFrame.addView(animating_view);
 		
 		try {
-			mWM.addView(mFrame, LAYOUT_PARAM);
+			if (Build.VERSION.SDK_INT >= 19) {
+				/* On Kitkat onwards, MATCH_PARENT by default doesn't fill
+				 * up the entire screen even though it has the permissions
+				 * to overlay everything.
+				 * We find the height and width and set them accordingly.
+				 * Furthermore, Gravity.CENTER seems to be slightly above 
+				 * the actual center. Thus, showing a few pixels of the 
+				 * nav bar. Setting to TOP | LEFT fixes that. */
+				DisplayMetrics displayMetrics = new DisplayMetrics();
+				mWM.getDefaultDisplay().getRealMetrics(displayMetrics);
+				
+				WindowManager.LayoutParams params = new WindowManager.LayoutParams();
+				params.copyFrom(LAYOUT_PARAM);
+				params.width = displayMetrics.widthPixels;
+				params.height = displayMetrics.heightPixels;
+				params.gravity = Gravity.TOP | Gravity.LEFT;
+				
+				mWM.addView(mFrame, params);
+			} else {
+				mWM.addView(mFrame, LAYOUT_PARAM);
+			}
 			animateView();
 		} catch (Exception e) {
 			Utils.log("(ScreenOffAnim) Error adding view to WindowManager", e);
